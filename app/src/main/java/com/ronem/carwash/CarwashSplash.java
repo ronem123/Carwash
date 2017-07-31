c85f4c33929101efb1f23fba7de93e74440b9b2e
Ram Mandal <ram.cp18113@gmail.com>
2017-07-31 17:47:36 +0545

    Project setup

---
+++ app/src/main/java/com/ronem/carwash/CarwashSplash.java
@@ -0,0 +1,13 @@
+package com.ronem.carwash;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CarwashSplash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_carwash_splash);
+    }
+}
