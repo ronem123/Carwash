a91ed21e3cc4e1faa23ee80fb486062dd0c570f9
ram <ram.cp18113@gmail.com>
2017-08-01 07:54:23 +0545

    login create account

--- app/src/main/java/com/ronem/carwash/view/LoginRegisterActivity.java
+++ app/src/main/java/com/ronem/carwash/view/LoginRegisterActivity.java
@@ -3,9 +3,14 @@ package com.ronem.carwash.view;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import com.ronem.carwash.R;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 /**
@@ -13,10 +18,20 @@ import butterknife.ButterKnife;
  */
 
 public class LoginRegisterActivity extends AppCompatActivity {
+    @Bind(R.id.create_account_layout)LinearLayout createAccLayout;
+    @Bind(R.id.login_layout)LinearLayout loginLayout;
+
+    @Bind(R.id.edt_full_name)EditText edtFullName;
+    @Bind(R.id.edt_email)EditText edtEmail;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.login_register_layout);
         ButterKnife.bind(this);
     }
