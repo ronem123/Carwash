a576c28deb368ecd91318e2d26257d8d73482afd
ram <ram.cp18113@gmail.com>
2017-08-12 01:58:46 +0545

    One signal setup

--- app/src/main/java/com/ronem/carwash/view/CarwashApplication.java
+++ app/src/main/java/com/ronem/carwash/view/CarwashApplication.java
@@ -3,6 +3,8 @@ package com.ronem.carwash.view;
 import android.app.Application;
 
 import com.activeandroid.ActiveAndroid;
+import com.onesignal.OneSignal;
+import com.ronem.carwash.onesignal.OneSignalOpenHandler;
 
 /**
  * Created by ronem on 8/8/17.
@@ -14,6 +16,10 @@ public class CarwashApplication extends Application {
     public void onCreate() {
         super.onCreate();
         ActiveAndroid.initialize(getApplicationContext());
+        OneSignal.startInit(this)
+                .setNotificationOpenedHandler(new OneSignalOpenHandler(getApplicationContext()))
+                .init();
+
     }
 
     @Override
